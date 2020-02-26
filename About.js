49a40524ad32ea03ddb5b945df6d2c8612d3bc4c
MD JOHIRUL ISLAM <52367779+johir-bd@users.noreply.github.com>
2020-02-26 17:02:02 +0600

    Add files via upload

---
+++ About.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+class About extends Component {
+  render() {
+    return (
+      <div>
+        <h1>About</h1>
+        <p>This is About page.</p>
+      </div>
+    );
+  }
+}
+
+export default About;
